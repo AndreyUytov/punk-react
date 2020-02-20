c1ebb5dddd365df82b51326aa787a37869031461
Andrew <uytov1991@gmail.com>
2020-02-20 16:06:44 +0300

    action-creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,62 @@
-export const REQUEST_
+export const REQUEST_BEERS = 'REQUEST_BEERS'
+export const SUCCESS_BEERS = 'SUCCESS_BEERS'
+export const FAILURE_BEERS = 'FAILURE_BEERS'
+export const SELECT_PAGE = 'SELECT_PAGE'
+
+export function selectPage (page) {
+    return {
+        type: SELECT_PAGE,
+        page
+    }
+}
+
+function requestBeers (page) {
+    return {
+        type: REQUEST_BEERS,
+        page
+    }
+}
+
+function successBeers (page, data) {
+    return {
+        type: SUCCESS_BEERS,
+        page,
+        data: JSON.parse(data)
+    }
+}
+
+function failureBeers (error) {
+    return {
+        type: FAILURE_BEERS,
+        error
+    }
+}
+
+function fetchBeers (page) {
+    return dispatch => {
+        dispatch(requestBeers(page))
+        return fetch(`https://api.punkapi.com/v2/beers?page=${page}&per_page=3`)
+            .then(res => res.json())
+            .then(json => dispatch(successBeers(page, json)))
+            .catch(err => failureBeers(err))
+    }
+}
+
+function shouldFetchBeers (state, page) {
+    const beers = state.beersByPage[page]
+    if (!beers) {
+        return true
+      } else if (beers.isFetching) {
+        return false
+      } else {
+        return beers.didInvalidate
+      }
+}
+
+export function fetchBeersIfNeeded (page) {
+    return (dispatch, getState) => {
+        if (shouldFetchBeers(getState(), page)) {
+            return dispatch(fetchBeers(page))
+        }
+    }
+}
